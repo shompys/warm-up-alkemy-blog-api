27096b8f6d6e50390ebbc64ec896c7fbcfcb2d05
jonathan <jonathangomezit@gmail.com>
2021-04-21 13:41:49 -0300

    Creation of the POSTS model, controller and methods

--- src/api/posts/posts.controllers.js
+++ src/api/posts/posts.controllers.js
@@ -1,39 +1,112 @@
 import Posts from './posts.models';
 
 export const createPosts = async (req, res, next) => {
-    const {titulo, contenido, imagen, categoria, created_at} = req.body;
 
-    // try{
-    //     const post = await Posts.create({
-    //         title: titulo,
-    //         content: contenido,
-    //         image: imagen,
-    //         category: categoria
-    //     })
-    //     console.log(res)
-    //     res.json(res)
-    // }catch(err){
-    //     console.log('estamos en catch --:' + err)
-    // }
+    const {titulo, contenido, imagen, categoria } = req.body;
+
+    try{
+
+        const postCreated = await Posts.create({
+            title: titulo,
+            content: contenido,
+            image: imagen,
+            category: categoria
+        })
+        
+        res.status(200).json(postCreated)
+
+    }catch(err){
+        
+        res.status(400).json({status: 400, msg: 'Error'});
+        
+    }
     
 }
 export const getPosts = async (req, res, next) => {
-    // Posts.findAll()
-    res.json('hola desde controllers getPosts')
 
+    try{
+
+        const postsSearchs = await Posts.findAll({
+            
+            attributes:['id', 'title', 'image', 'category', 'createdAt'],
+            order:[['createdAt', 'DESC']]
+            
+        })
+        
+        if (postsSearchs.length === 0) throw Error('The resources do not exist')
+
+        res.status(200).json(postsSearchs)
+
+    }catch(error){
+
+        res.status(400).json({status: 400, msg: error.message})
+        
+
+    }
+    
 }
 export const getPostsById = async (req, res, next) => {
+
+    try{
+
+        const postSearch = await Posts.findByPk(req.params.id)
+        //generamos error null cuando no existe id
+        if (postSearch === null) throw new Error('The resource does not exist')
+        res.status(200).json(postSearch)
+
+    }catch(error){
+
+        res.status(404).json({status: 404, msg: error.message})
+
+    }
     
-    res.json(`Hola desde getPostsById: ${req.params.id}`)
 
 }
 export const updateUserById = async (req, res, next) => {
 
-    res.json('Hola desde updateuserById: ' + req.params.id);
+    const {titulo, contenido, imagen, categoria } = req.body;
+
+    try{
+
+        const postUpdated = await Posts.update({
+            title: titulo,
+            content: contenido,
+            image: imagen,
+            category: categoria
+        },{
+            where:{
+                id: req.params.id
+            }
+        })
+
+        if(postUpdated[0] === 0) throw new Error(`No changes were made or the resource does not exist`);
+        res.status(200).json({status: 200, msg: 'Updated resource'});
+
+    }catch(error){
+        
+        res.status(400).json({status: 400, msg: error.message})
+        
+    }
+    
 
 }
 export const deleteUserById = async (req, res, next) => {
 
-    res.json('Hola desde deleteUserById: ' + req.params.id);
+    try{
+
+        const postDeleted = await Posts.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+
+        if(postDeleted === 0 ) throw Error('The resource does not exist')
+        
+        res.status(200).json({status:200, msg: 'Resource deleted'})
+
+    }catch(error){
+        
+        res.status(400).json({status: 400, msg: error.message})
+    }
 
 }
--- src/api/posts/posts.models.js
+++ src/api/posts/posts.models.js
@@ -1,17 +1,17 @@
-// import sequelize from '../../db'; //Probar si funciona sin importarlo aquí
-// import { Model, DataTypes} from 'sequelize';
+import sequelize from '../../db'; //Probar si funciona sin importarlo aquí
+import { Model, DataTypes} from 'sequelize';
 
-// class Posts extends Model {}
-// Posts.init({
+class Posts extends Model {}
+Posts.init({
 
-//     title: { type: DataTypes.STRING, unique: true },
-//     content: { type: DataTypes.STRING },
-//     image: { type: DataTypes.STRING },
-//     category: { type: DataTypes.STRING}
-//     // created_at : { type: DataTypes.DATE, defaultValue: DataTypes.NOW } Por defecto sequelize me genera createdAT y updatedAT
-// },
-// {
-// sequelize, modelName: 'posts'
-// })
+    title: { type: DataTypes.STRING },
+    content: { type: DataTypes.TEXT },
+    image: { type: DataTypes.STRING },
+    category: { type: DataTypes.STRING}
 
-// export default Posts;
+},
+{
+sequelize, modelName: 'posts'
+})
+
+export default Posts;
